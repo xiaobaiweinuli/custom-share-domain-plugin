b395d3bef40c9a6fe98c49947edd59a100f6b37f
星霜笔记 <94781176+xiaobaiweinuli@users.noreply.github.com>
2025-09-16 00:57:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,21 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     preact(),
     ...blinkoPlugin()
-  ]
+  ],
+  build: {
+    minify: "terser", // 显式设置使用terser进行压缩
+    terserOptions: {
+      mangle: {
+        reserved: ['rt', 'fe', 'withSettingPanel'], // 保留这些变量名不被混淆
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  esbuild: {
+    minifyIdentifiers: false, // 不压缩标识符
+  }
 }));
